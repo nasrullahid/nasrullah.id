fcf1508b8bd573d0be866e2f6a76096340949325
Nasrullah <halo.nasrullah@gmail.com>
2024-04-18 16:45:42 +0800

    first commit

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Nasrullah | Fullstack Web Developer";
+export const SITE_DESCRIPTION =
+  "Saya sudah pengalaman 10 tahun lebih bekerja sebagai Web Developer profesional, teliti dalam bekerja dan cepat beradaptasi dengan lingkungan.";
+export const GENERATE_SLUG_FROM_TITLE = true;
+export const TRANSITION_API = true;
