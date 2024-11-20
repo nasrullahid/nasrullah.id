321ec53d939a66ebd4b2f309f4d615bf9076fb0d
Nasrullah <halo.nasrullah@gmail.com>
2024-11-20 18:19:37 +0800

    update pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'nasrullah.id',
-      script: 'npm i && npm run build && npm run preview',
+      script: 'npm i && npm run build && npm run preview --host 0.0.0.0 --port 4000',
       post_update: [
         'echo App has been updated, running npm install...',
         'npm install',
